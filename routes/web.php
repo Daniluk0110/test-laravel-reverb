6d0523e77b0600a5fbf06f234919d7ebe854fd53
Daniel S <sech-daniil@yandex.ru>
2025-12-26 16:48:11 +0300

    add likes route and AJAX handler for broadcasting reactions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Events\TestNotification;
 use Illuminate\Support\Facades\Route;
+use App\Events\ReactionSent;
+use Illuminate\Http\Request;
 
 Route::get('/fire', function () {
     // Отправляем событие с текстом
@@ -12,4 +14,20 @@ Route::get('/fire', function () {
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Страница с кнопками
+Route::get('/likes', function () {
+    return view('likes');
+});
+
+// Обработка нажатия (AJAX)
+Route::post('/likes', function (Request $request) {
+    // Валидация (просто чтобы не слали мусор)
+    $request->validate(['type' => 'required|string']);
+
+    // Отправляем событие всем
+    ReactionSent::dispatch($request->type);
+
+    return response()->json(['status' => 'ok']);
 });
